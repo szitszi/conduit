a298042edac717f1a6e763d71d719f55c7b42ecc
Szitar Szilvia <szitar.szilvia@gmail.com>
2021-07-15 18:26:56 +0200

    tc_5_pages.py - modified

--- selenium_python_tests/tc_5_pages.py
+++ selenium_python_tests/tc_5_pages.py
@@ -19,6 +19,7 @@ def registration_process():
         # time.sleep(2)
     driver.find_element_by_tag_name("button").click()
 
+
 registration_process()
 
 time.sleep(2)
@@ -31,17 +32,17 @@ time.sleep(2)
 # ---------Log out-----------
 driver.find_element_by_xpath("//a[@active-class='active']").click()
 
-
 time.sleep(2)
 
 
 # ------Sign in---------
 def login_process():
     driver.find_element_by_xpath("//a[@href='#/login']").click()
-    for i in range(len(input_data)-1):
-        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(input_data[i+1])
+    for i in range(len(input_data) - 1):
+        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(input_data[i + 1])
         time.sleep(1)
     driver.find_element_by_tag_name("button").click()
+
 
 login_process()
 
@@ -49,43 +50,12 @@ login_process()
 
 time.sleep(5)
 
-
-# driver.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[1]/a").click()
-# time.sleep(1)
-# driver.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[2]/a").click()
-# time.sleep(1)
-# driver.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[3]/a").click()
-# time.sleep(1)
-# driver.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[4]/a").click()
-# time.sleep(1)
-# driver.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[5]/a").click()
-# time.sleep(1)
-# driver.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[6]/a").click()
-# time.sleep(1)
-
-
-pages = []
-for i in range(number_of_pages):
-    oldal = driver.find_element_by_xpath(f"//*[@id='app']/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[{i+1}]/a")
-    pages.append(oldal)
-
+pagination = driver.find_element_by_xpath("//ul[@class='pagination']")
+pages = driver.find_elements_by_class_name("page-link")
 print(len(pages))
 
 for page in pages:
     page.click()
-    time.sleep(1.5)
-
-
-
-# pagination = driver.find_element_by_xpath("//ul[@class='pagination']")
-# # pages = pagination.find_elements_by_xpath("//li[starts with(@data-test,'page-link-')]")
-# pages = pagination.find_elements_by_xpath("//li/a")
-# # //tag[starts with(attribute, ‘value’)]
-# print(len(pages))
-#
-# for page in pages:
-#     page.click()
-#     time.sleep(1)
-
+    time.sleep(1)
 
 driver.close()
