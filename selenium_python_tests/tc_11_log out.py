5f23e0cecf02b4827235b6e8858f6529ec7eb4e0
Szitar Szilvia <szitar.szilvia@gmail.com>
2021-07-26 16:31:06 +0200

    update of 11 test

--- selenium_python_tests/tc_11_log out.py
+++ selenium_python_tests/tc_11_log out.py
@@ -1,47 +1,72 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import time
+import random
+import string
 
-driver = webdriver.Chrome()
+opt = Options()
+opt.headless = False
+# options.add_argument('--disable-gpu')
 
-driver.get('http://localhost:1667/')
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
+driver.set_window_rect(1200, 400, 1300, 1000)
 
-input_data = ["Sz", "Sz55@sz.hu", "Sz123456"]
+try:
+    driver.get('http://localhost:1667/')
 
-
-# Sign up
-def registration_process():
-    driver.find_element_by_xpath("//a[@href='#/register']").click()
-    # time.sleep(2)
-    for i in range(len(input_data)):
-        driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(input_data[i])
-        # time.sleep(2)
-    driver.find_element_by_tag_name("button").click()
-
-registration_process()
-
-time.sleep(2)
-
-# Accept of welcome message
-driver.find_element_by_xpath("//div[@tabindex='-1']/div/div[4]/div/button").click()
-
-time.sleep(2)
-
-navbar_item_2_logged_in = driver.find_element_by_xpath("//ul[@class='nav navbar-nav pull-xs-right']/li[2]/a/i").text
-print(navbar_item_2_logged_in)
-number_of_items_logged_in = len(driver.find_elements_by_xpath("//ul[@class='nav navbar-nav pull-xs-right']/li"))
-print(number_of_items_logged_in)
-# Log out
-driver.find_element_by_xpath("//a[@active-class='active']").click()
-
-# navbar_item_2_logged_out = ("//a[@href='#/editor']/i").text
-navbar_item_2_logged_out = driver.find_element_by_xpath("//ul[@class='nav navbar-nav pull-xs-right']/li[2]/a/i").text
-print(navbar_item_2_logged_out)
-number_of_items_logged_out = len(driver.find_elements_by_xpath("//ul[@class='nav navbar-nav pull-xs-right']/li"))
-print(number_of_items_logged_out)
+    input_data = ["".join([random.choice(string.ascii_lowercase) for _ in range(5)]),
+                  f"{random.choice(string.ascii_lowercase)}{random.randint(10, 1000)}@mail.hu", "Pw123456"]
 
 
-time.sleep(2)
+    # -----------Sign up----------
+    def registration_process():
+        driver.find_element_by_xpath("//a[@href='#/register']").click()
+        for i in range(len(input_data)):
+            driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(input_data[i])
+        driver.find_element_by_tag_name("button").click()
 
-### navbar_item_2_logged_in and navbar_item_2_logged_out are empty because of before/after, selenium does not work, we need css!?!
 
-driver.close()
+    registration_process()
+
+    time.sleep(2)
+
+    # --------Accept of welcome message----------
+    driver.find_element_by_xpath("//div[@tabindex='-1']/div/div[4]/div/button").click()
+
+    time.sleep(2)
+
+    # --------Check of login name---------
+    login_name = driver.find_element_by_xpath("//li[@class='nav-item'][4]/a")
+    print(login_name.text)
+    assert login_name.text == input_data[0]
+
+    # --------Log out process---------
+    """check navbar items before log out"""
+    navbar_elements_before = driver.find_elements_by_xpath("//*[@id='app']/nav/div/ul/li/a")
+    # print(len(navbar_elements_before))
+    for element in navbar_elements_before:
+        print(element.text)
+    assert navbar_elements_before[3].text == input_data[0]
+
+    log_out_link = driver.find_element_by_xpath("//a[@active-class='active']")
+    log_out_link.click()
+
+    # if log_out_link.is_enabled():
+    #     print("log out was clicked")
+    #     log_out_link.click()
+    # else:
+    #     print("error")
+
+    """check navbar items after log out"""
+    navbar_elements_after = driver.find_elements_by_xpath("//*[@id='app']/nav/div/ul/li/a")
+    # print(len(navbar_elements_after))
+    for element in navbar_elements_after:
+        print(element.text)
+    assert navbar_elements_after[2].text == "Sign up"
+
+    time.sleep(2)
+
+
+finally:
+    driver.close()
