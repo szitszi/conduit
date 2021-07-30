2b04ddad89b14c7ba40bf6ed3a46a575f79b614a
Szitar Szilvia <szitar.szilvia@gmail.com>
2021-07-30 12:22:09 +0200

    update of 3 test

--- selenium_python_tests/tc_3_gdpr.py
+++ selenium_python_tests/tc_3_gdpr.py
@@ -14,18 +14,36 @@ driver.get('http://localhost:1667/')
 
 time.sleep(2)
 
-# ----------Accept cookies---------
 
-driver.find_element_by_xpath("//button[@class='cookie__bar__buttons__button cookie__bar__buttons__button--accept']").click()
+# accept_button = driver.find_element_by_xpath("//button[@class='cookie__bar__buttons__button cookie__bar__buttons__button--accept']")
+# decline_button = driver.find_element_by_xpath("//button[@class='cookie__bar__buttons__button cookie__bar__buttons__button--decline']")
+# accept_button = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]/div')
+# decline_button = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[1]/div')
+accept_button = driver.find_element_by_xpath('/html/body/div/footer/div/div/div/div[2]/button[2]/div')
+decline_button = driver.find_element_by_xpath('/html/body/div/footer/div/div/div/div[2]/button[1]/div')
+
+assert accept_button.is_enabled()
+assert decline_button.is_enabled()
+
+decline_button.click()
 
 time.sleep(2)
 
+# driver.refresh()
+driver.close()
+# driver.quit()
+
+time.sleep(2)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
+driver.set_window_rect(1200, 400, 1300, 1000)
+driver.get('http://localhost:1667/')
+time.sleep(1)
 
 
-# -----------Decline cookies---------
+accept_button = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]/div')
+decline_button = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[1]/div')
 
-# driver.find_element_by_xpath("//button[@class='cookie__bar__buttons__button cookie__bar__buttons__button--decline']").click()
-#
-# time.sleep(2)
-#
+assert not accept_button.is_enabled()
+assert not decline_button.is_enabled()
+
 driver.close()
